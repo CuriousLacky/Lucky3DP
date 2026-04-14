b01cca70ccf7db2068a9e7fd4784c4e95bae5a43
CuriousLacky <lackysahuji1@gmail.com>
2026-04-14 23:32:42 +0530

    Update prisma.ts

--- lucky3dp/lib/prisma.ts
+++ lucky3dp/lib/prisma.ts
@@ -7,10 +7,6 @@ declare global {
 }
 
 const prisma = global.prisma ?? new PrismaClient();
-
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-const prisma = global.prisma ?? new PrismaClient();
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
 export default prisma;
 export { prisma }; // <-- Add this line
